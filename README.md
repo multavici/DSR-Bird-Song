e94bc68dc7b56c835e52a99b0855199817632341
Falk Van der Meirsch <falk@codedor.be>
2019-02-13 18:49:43 +0100

    clean up code + add download script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # DSR-Bird-Song  
 
 Portfolio project for DSR. Clasification of bird songs.
+
+Running a job: 
+
+```
+paperspace jobs create --command <command> --container <container> --apiKey <apiKey>
+````
+
--- model.py
+++ model.py
@@ -47,4 +47,34 @@ def geni_df():
     df = pd.read_sql(sql=query, con=conn)
     return df
 
-print(geni_df())
+
+def german_species_df():
+    query = '''SELECT t.bird_order, t.family, t.genus, t.species, t.german,
+            COUNT(r.id), MIN(r.id), MAX(r.id)
+        FROM taxonomy AS t
+        JOIN recordings AS r ON t.id = r.taxonomy_id
+        WHERE t.german = 1.0
+        GROUP BY t.bird_order, t.family, t.genus, t.species
+        ORDER BY COUNT(r.id) DESC'''
+    df = pd.read_sql(sql=query, con=conn)
+    return df
+
+
+def all_recordings_df():
+    query = '''SELECT t.bird_order, t.family, t.genus, t.species, t.german,
+            r.id
+        FROM taxonomy AS t
+        JOIN recordings AS r ON t.id = r.taxonomy_id
+        ORDER BY r.id'''
+    df = pd.read_sql(sql=query, con=conn)
+    return df
+
+
+def german_recordings_df():
+    query = '''SELECT t.bird_order, t.family, t.genus, t.species, r.id
+        FROM taxonomy AS t
+        JOIN recordings AS r ON t.id = r.taxonomy_id
+        WHERE t.german = 1.0
+        ORDER BY r.id'''
+    df = pd.read_sql(sql=query, con=conn)
+    return df
