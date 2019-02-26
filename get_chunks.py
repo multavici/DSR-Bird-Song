2628e1fb48d6269e7e731be36c1e6e17c15e5125
Falk Van der Meirsch <falk@codedor.be>
2019-02-26 15:54:12 +0100

    try to find out default execution folder for jobs

--- get_chunks.py
+++ get_chunks.py
@@ -8,9 +8,10 @@ def get_records_from_classes(class_ids, seconds_per_class):
     Returns an amount of recordings so that the total seconds of signal
     is equal to or more than the argument seconds_per class
     """
-    db_dir = os.path.join(os.getcwd(), 'storage', 'db.sqlite')
-    print(db_dir)
-    conn = sqlite3.connect(db_dir)
+    #db_dir = os.path.join(os.getcwd(), 'storage', 'db.sqlite')
+    #print(db_dir)
+    #conn = sqlite3.connect(db_dir)
+    conn = sqlite3.connect('/storage/db.sqlite')
     c = conn.cursor()
 
     result = []
