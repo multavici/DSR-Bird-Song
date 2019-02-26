5b53441c84cd2d043d995d8b79aed3f392b2e295
bimtauer <ti7822ba-s@student.lu.se>
2019-02-26 13:36:56 +0100

    Setup import_test

--- import_test.py
+++ import_test.py
@@ -1,31 +1,58 @@
 
 # Test Run
 import pandas as pd
-from Spectrogram.spectrograms import mel_s
+from Spectrogram.spectrograms import mel_s, stft_s
 from torch.utils.data import DataLoader
-df = pd.read_csv('Testing/test_df.csv')
+from Datasets.static_dataset import SoundDataset
+from get_chunks import get_records_from_classes
+from models.bulbul import BulBul
 
 
-params = {'batchsize' : 64, 
+
+##########################################################################
+class_ids =  [5096, 4996, 4993, 4990, 4980]
+df = get_records_from_classes(class_ids, 100)
+
+# Check sample distribution:
+df.groupby('label').agg({'total_signal':'sum'})
+
+# Split into train and test
+
+##########################################################################
+
+
+
+
+##########################################################################
+BATCHSIZE = 64
+
+# Parameters for sample loading
+params = {'batchsize' : BATCHSIZE, 
           'window' : 1500, 
           'stride' : 500, 
           'spectrogram_func' : mel_s, 
           'augmentation_func' : None}
 
 
-from Datasets.static_dataset import SoundDataset
+ds_test = SoundDataset(df_test, **params)
+dl_test = DataLoader(ds_test, BATCHSIZE)
 
-ds = SoundDataset(df, **params)
+ds_train = SoundDataset(df_train, **params)
+dl_train = DataLoader(ds_train, BATCHSIZE)
 
-len(ds)
 
-dl = DataLoader(ds, 64)
+
+
+
+##########################################################################
+time_axis = ds_test[0][0].shape[1]
+freq_axis = ds_test[0][0].shape[0]
+
+net = BulBul(time_axis, freq_axis, len(class_ids))
+
+
 
 
 for batch in dl:
-    print(batch[0].shape, batch[1])
+    
 
-
-# To visualize training samples:
-import matplotlib.pyplot as plt
-plt.imshow(ds[1][0])
