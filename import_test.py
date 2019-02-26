a3dab198112cad37b7f0984d0cf884ebfb857e6d
My_Name <stynshrm@gmail.com>
2019-02-26 14:52:04 +0100

    Added training loop

--- import_test.py
+++ import_test.py
@@ -7,6 +7,14 @@ from Datasets.static_dataset import SoundDataset
 from get_chunks import get_records_from_classes
 from models.bulbul import BulBul
 
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.metrics import accuracy_score, log_loss
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 ##########################################################################
@@ -25,6 +33,8 @@ df.groupby('label').agg({'total_signal':'sum'})
 
 ##########################################################################
 BATCHSIZE = 64
+OPTIMIZER = 'Adam'
+EPOCHS = 10
 
 # Parameters for sample loading
 params = {'batchsize' : BATCHSIZE, 
@@ -51,8 +61,72 @@ freq_axis = ds_test[0][0].shape[0]
 net = BulBul(time_axis, freq_axis, len(class_ids))
 
 
+criterion = nn.CrossEntropyLoss()
+#optimizer = optim.SGD(Bulbul.parameters(), lr=0.0001, momentum=0.9)
+optimizer = optim.Adam(Bulbul.parameters(), lr = 0.0001)
 
 
-for batch in dl:
+def evaluate_model(model, test_loader, print_info=False):
+    with torch.no_grad():
+        model.eval()
+        collect_results = []
+        collect_target = []
+        for batch in test_loader:
+            X, y = batch
+            X = X.to(DEVICE)
+            y = y.to(DEVICE).detach().cpu().numpy()
+            pred = net(X)
+
+            collect_results.append(F.softmax(pred).detach().cpu().numpy())
+            collect_target.append(y) 
+    
+        preds_proba = np.concatenate(collect_results)
+        preds = preds_proba.argmax(axis=1)
+        
+        targets = np.concatenate(collect_target)
+        
+        ll = log_loss(targets, preds_proba)
+        acc = accuracy_score(targets, preds)
+        if print_info:
+            print("test log-loss: {}".format(ll))
+            print("overall accuracy:  {}".format(acc))
+            #print(classification_report(targets, preds))
+        model.train()
+        
+        return ll, acc
+
+
+
+collect_metrics = []
+collect_loss = []
+
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(dl_train):
+        # get the inputs
+        X, y = data
+            
+        X = X.to(DEVICE)
+        y = y.to(DEVICE)
+
+ 
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        y_pred = net(X)
+        loss = criterion(y_pred, y)
+        loss.backward()
+        optimizer.step()
+    
+    lltest, acctest = evaluate_model(net, dl_test)
+    lltrain, acctrain = evaluate_model(net, dl_train)
+
+    collect_metrics.append((lltest, lltrain, acctest, acctrain))
+    print("test: loss: {}  acc: {}".format(lltest, acctest))
+    print("train: loss: {}  acc: {}".format(lltrain, acctrain))
+
+print('Finished Training')
+
     
 
