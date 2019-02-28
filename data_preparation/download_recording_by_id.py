94196776393d6be4f1d131c52465ccc9468c89b2
Falk Van der Meirsch <falk@codedor.be>
2019-02-28 13:59:43 +0100

    add extra download try if first one fails

--- data_preparation/download_recording_by_id.py
+++ data_preparation/download_recording_by_id.py
@@ -11,5 +11,12 @@ def download_recordings(ids):
                 os.path.join(files_dir, str(id_rec) + ".mp3"))
             print(f'file {id_rec} downloaded')
         except urllib.error.HTTPError:
-            print(f'file {id_rec} not found')
+            print(f'file {id_rec} not found, trying again')
+            try: 
+                urllib.request.urlretrieve("http://www.xeno-canto.org/" 
+                    + str(id_rec) + "/download",
+                    os.path.join(files_dir, str(id_rec) + ".mp3"))
+                print(f'file {id_rec} downloaded')
+            except:
+                print(f'file {id_rec} not found')
             pass
