b901e934020c01c59fcbd50adb8ad84eec6c4f7c
bimtauer <ti7822ba-s@student.lu.se>
2019-03-01 15:24:58 +0100

    First run 79%, 89% Acc, conf matrix plot included

--- import_test.py
+++ import_test.py
@@ -51,7 +51,7 @@ print('{"chart": "test accuracy", "axis": "epochs"}')
 
 BATCHSIZE = 32
 OPTIMIZER = 'Adam'
-EPOCHS = 10
+EPOCHS = 50
 CLASSES = 10
 
 
@@ -75,7 +75,7 @@ net = Bulbul(time_axis=time_axis, freq_axis=freq_axis, no_classes=CLASSES)
 
 criterion = nn.CrossEntropyLoss()
 #optimizer = optim.SGD(Bulbul.parameters(), lr=0.0001, momentum=0.9)
-optimizer = optim.Adam(net.parameters(), lr = 0.0001)
+optimizer = optim.Adam(net.parameters(), lr = 0.001)
 
 
 def evaluate_model(model, test_loader, print_info=False):
@@ -148,7 +148,7 @@ for epoch in range(EPOCHS):  # loop over the dataset multiple times
 
 print('Finished Training')
 total_time = time.time() - start_time
-
+"""
 log = {
     'date': time.strftime('%d/%m/%Y'),
     'no_classes': CLASSES,
@@ -164,4 +164,64 @@ log = {
 
 }
 json.dump(log, open('/storage/runlog.txt', 'w+'))
+"""
+
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
+
+y_test = []
+y_pred = []
+for i in range(len(ds_test)):
+    x, y_true = ds_test[i]
+    x = torch.Tensor(x).float().unsqueeze(dim = 0)
+    y_p = net(x)
+    y_test.append(y_true)
+    y_pred.append(np.argmax(y_p.detach().numpy()))
+
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=list(range(CLASSES)), normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
+
+
+
 
