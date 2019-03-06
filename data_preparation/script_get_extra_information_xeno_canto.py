55c3d4f92fb2a50acaaa98d7fb14e008bab44c2c
Falk Van der Meirsch <falk@codedor.be>
2019-03-06 10:58:05 +0100

    try fix for paperspace

--- data_preparation/script_get_extra_information_xeno_canto.py
+++ data_preparation/script_get_extra_information_xeno_canto.py
@@ -22,35 +22,34 @@ recordings = c.fetchall()
 
 def download(input_tuple):
     rec_id, xeno_canto_id = input_tuple
-    try: 
-        tables = pd.read_html('https://www.xeno-canto.org/' + str(xeno_canto_id))
-        circumstances, mp3_info, processed_info = tables[2], tables[3], tables[4]
-        d = {
-            'id': rec_id,
-            'xeno_canto_id': xeno_canto_id,
-            'background_species': circumstances.iloc[8,1], 
-            'duration': mp3_info.iloc[0,1], 
-            'sr': mp3_info.iloc[1,1], 
-            'bitrate': mp3_info.iloc[2,1], 
-            'channels': mp3_info.iloc[3,1], 
-            'type': processed_info.iloc[0, 1], 
-            'volume': processed_info.iloc[1, 1], 
-            'speed': processed_info.iloc[2, 1], 
-            'pitch': processed_info.iloc[3, 1], 
-            'length': processed_info.iloc[4, 1], 
-            'no_notes': processed_info.iloc[5, 1], 
-            'variable': processed_info.iloc[6, 1]
-        }
-        with open(STORAGE_DIR + str(threading.get_ident()) ,'a+') as f:
-            f.write(json.dumps(d))
-        return d
-    except:
-        print('error: ', rec_id)
-        return None
+    #try: 
+    tables = pd.read_html('https://www.xeno-canto.org/' + str(xeno_canto_id))
+    circumstances, mp3_info, processed_info = tables[2], tables[3], tables[4]
+    d = {
+        'id': rec_id,
+        'xeno_canto_id': xeno_canto_id,
+        'background_species': circumstances.iloc[8,1], 
+        'duration': mp3_info.iloc[0,1], 
+        'sr': mp3_info.iloc[1,1], 
+        'bitrate': mp3_info.iloc[2,1], 
+        'channels': mp3_info.iloc[3,1], 
+        'type': processed_info.iloc[0, 1], 
+        'volume': processed_info.iloc[1, 1], 
+        'speed': processed_info.iloc[2, 1], 
+        'pitch': processed_info.iloc[3, 1], 
+        'length': processed_info.iloc[4, 1], 
+        'no_notes': processed_info.iloc[5, 1], 
+        'variable': processed_info.iloc[6, 1]
+    }
+    with open(STORAGE_DIR + str(threading.get_ident()) ,'a+') as f:
+        f.write(json.dumps(d))
+    #except:
+    #    print('error: ', rec_id)
+    #    return None
 
 
 pool = ThreadPool(24)
-rows = pool.map(download, recordings)
+pool.map(download, recordings)
 
 """
 df = pd.DataFrame(rows)
