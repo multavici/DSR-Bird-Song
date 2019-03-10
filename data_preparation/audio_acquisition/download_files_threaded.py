b3c570cc5633fb6922ec0cfd26305e6a33ebaded
Falk Van der Meirsch <falk@codedor.be>
2019-03-10 10:36:09 +0100

    add docs, move and rename

--- data_preparation/audio_acquisition/download_files_threaded.py
+++ data_preparation/audio_acquisition/download_files_threaded.py
@@ -1,3 +1,8 @@
+'''
+This script downloads all recordings of german species from the Xeno-Canto
+website.
+'''
+
 import sqlite3
 import urllib.request
 import os
@@ -22,16 +27,18 @@ query = '''SELECT r.id, r.file
 
 recordings = c.execute(query).fetchall()
 
+
 def download(input_tuple):
     rec_id, path = input_tuple
     try:
         resp = urllib.request.urlretrieve("http:" + path,
-            STORAGE_DIR + str(rec_id) + ".mp3")
+                                          STORAGE_DIR + str(rec_id) + ".mp3")
         assert resp[1]['Content-Type'] == 'audio/mpeg', f'file {rec_id} not available'
         print(f'file {rec_id} downloaded')
     except urllib.error.HTTPError:
         print(f'file {rec_id} not found, HTTPError')
         pass
+
 
 pool = ThreadPool(24)
 pool.map(download, recordings)
