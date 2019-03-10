b1321e2e489a3a19287ebac4f46b43e508cb4c90
bimtauer <ti7822ba-s@student.lu.se>
2019-03-10 16:23:31 +0100

    Change back to running loss and acc

--- birdsong/training/train.py
+++ birdsong/training/train.py
@@ -11,10 +11,11 @@ from .utils import printProgressBar
 
 
 def train(model, data_loader, epoch, optimizer, criterion, DEVICE):
+    n_correct = 0
+    n_total = 0
+    running_loss = 0.0
     
-    loss, n_correct, acc = 0., 0., 0.
     model.train()
-
     for batch_idx, (data, target) in enumerate(data_loader):
         data, target = Variable(data), Variable(target)
         data = data.float()
@@ -29,10 +30,14 @@ def train(model, data_loader, epoch, optimizer, criterion, DEVICE):
         optimizer.step()
 
         pred = output.data.max(1, keepdim=True)[1]
-        n_correct = pred.eq(target.data.view_as(pred)).cpu().sum().item()
-        acc = 100. * n_correct / len(data)
-
+        n_correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+        n_total += len(target)
+        running_loss += loss.item()
+        
+        current_loss = running_loss/(batch_idx+1)
+        current_acc = n_correct/n_total
+        
         printProgressBar(batch_idx + 1, len(data_loader),
                          prefix=f'Epoch: {epoch}',
-                         suffix=f'Batch Loss:{loss.item()} Batch Acc:{acc}',
+                         suffix=f'Running Loss:{current_loss:.5f}, Running Acc:{current_acc:.5f}',
                          length=50)
