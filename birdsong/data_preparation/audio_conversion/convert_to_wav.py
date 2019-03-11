8cecd00e4d9a5af0bbd8446334339a9a8229c199
Falk Van der Meirsch <falk@codedor.be>
2019-03-11 18:40:46 +0100

    add script to convert to wav

---
+++ birdsong/data_preparation/audio_conversion/convert_to_wav.py
@@ -0,0 +1,29 @@
+"""
+This script converts mp3 files from different sample rates and amount of channels
+to wav files with a fixed sample rate of 22050 and 1 channel (mono)
+"""
+
+import os
+import sqlite3
+import librosa
+from multiprocessing.pool import ThreadPool
+
+if 'HOSTNAME' in os.environ:
+    # script runs on server
+    INPUT_DIR = '/storage/step1/'
+    OUTPUT_DIR = '/storage/step1_wav/'
+else:
+    # script runs locally
+    INPUT_DIR = 'storage/step1/'
+    OUTPUT_DIR = 'storage/step1_wav/'
+
+mp3_files = os.listdir(INPUT_DIR)
+
+
+def convert(mp3_file):
+    y, sr = librosa.core.load(INPUT_DIR + mp3_file)
+    librosa.output.write_wav(OUTPUT_DIR + mp3_file[:-4] + '.wav', y, sr)
+
+
+pool = ThreadPool(24)
+pool.map(convert, mp3_files)
