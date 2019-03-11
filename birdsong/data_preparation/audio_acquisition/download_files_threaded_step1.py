06283fb4bdcf3edc35b1571fa61ec6ef272f7cd0
Falk Van der Meirsch <falk@codedor.be>
2019-03-11 17:46:41 +0100

    new file to download step1 mp3 files

---
+++ birdsong/data_preparation/audio_acquisition/download_files_threaded_step1.py
@@ -0,0 +1,44 @@
+'''
+This script downloads a selected subset of recordings of german species from the 
+Xeno-Canto website.
+'''
+
+import sqlite3
+import urllib.request
+import os
+from multiprocessing.pool import ThreadPool
+
+if 'HOSTNAME' in os.environ:
+    # script runs on server
+    STORAGE_DIR = '/storage/step1/'
+    DATABASE_DIR = '/storage/db.sqlite'
+else:
+    # script runs locally
+    STORAGE_DIR = 'storage/step1/'
+    DATABASE_DIR = 'storage/db.sqlite'
+
+conn = sqlite3.connect(DATABASE_DIR)
+c = conn.cursor()
+
+query = '''SELECT r.id, r.file
+    FROM taxonomy AS t
+    JOIN recordings AS r ON t.id = r.taxonomy_id
+    WHERE step1 = 1'''
+
+recordings = c.execute(query).fetchall()
+
+
+def download(input_tuple):
+    rec_id, path = input_tuple
+    try:
+        resp = urllib.request.urlretrieve("http:" + path,
+                                          STORAGE_DIR + str(rec_id) + ".mp3")
+        assert resp[1]['Content-Type'] == 'audio/mpeg', f'file {rec_id} not available'
+        print(f'file {rec_id} downloaded')
+    except urllib.error.HTTPError:
+        print(f'file {rec_id} not found, HTTPError')
+        pass
+
+
+pool = ThreadPool(24)
+pool.map(download, recordings)
