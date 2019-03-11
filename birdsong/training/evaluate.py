383e5f6d6cdb1915a5021e7cb58b8d333356e6e2
bimtauer <ti7822ba-s@student.lu.se>
2019-03-11 19:40:51 +0100

    Fixed train/test evaluation Acc/Loss & created script to train raw sound

--- birdsong/training/evaluate.py
+++ birdsong/training/evaluate.py
@@ -17,42 +17,36 @@ def evaluate(model, data_loader, criterion, num_classes, DEVICE):
     model.eval()
     model = model.to(DEVICE)
     loss = 0
-    correct = 0
-#    conf = np.ndarray((num_classes, num_classes))
+    n_correct = 0
+    n_total = 0
 
-    i = 0
     with torch.no_grad():
-        for data, target in data_loader:
-            
+        for batch_idx, (data, target) in enumerate(data_loader):
             data, target = Variable(data), Variable(target)
             data = data.float()
             data = data.to(DEVICE)
             target = target.to(DEVICE)
 
             output = model(data)
-            
-            #loss += F.cross_entropy(output, target, reduction='sum').data.item()   
-            loss +=criterion(output,target)
-            
+
+            loss += criterion(output, target).item()
+
             pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
-            if i == 0:
+            n_correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+            n_total += len(target)
+
+            if batch_idx == 0:
                 pred_cat = pred
                 targ_cat = target
-                i = 1
             else:
                 pred_cat = torch.cat((pred_cat, pred))
                 targ_cat = torch.cat((targ_cat, target))
 
     conf_matrix = calc_conf_mat(pred_cat, targ_cat, num_classes)
-    #from sklearn   
-    #conf_matrix = metrics.confusion_matrix(pred_cat.view(-1), targ_cat.view(-1)) 
-    
-    loss /= len(data_loader.dataset)
+    #from sklearn
+    #conf_matrix = metrics.confusion_matrix(pred_cat.view(-1), targ_cat.view(-1))
 
-    acc = 100. * correct / len(data_loader.dataset)
-        
-    print('Average loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)'.format(
-        loss, correct, len(data_loader.dataset), acc))
+    loss /= (batch_idx + 1)
+    acc = n_correct / n_total
 
-    return (loss.item(), acc), conf_matrix
+    return (loss, acc), conf_matrix
--- birdsong/training/train.py
+++ birdsong/training/train.py
@@ -14,7 +14,7 @@ def train(model, data_loader, epoch, optimizer, criterion, DEVICE):
     n_correct = 0
     n_total = 0
     running_loss = 0.0
-    
+
     model.train()
     model = model.to(DEVICE)
     for batch_idx, (data, target) in enumerate(data_loader):
@@ -34,10 +34,10 @@ def train(model, data_loader, epoch, optimizer, criterion, DEVICE):
         n_correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
         n_total += len(target)
         running_loss += loss.item()
-        
+
         current_loss = running_loss/(batch_idx+1)
         current_acc = n_correct/n_total
-        
+
         printProgressBar(batch_idx + 1, len(data_loader),
                          prefix=f'Epoch: {epoch}',
                          suffix=f'Running Loss:{current_loss:.5f}, Running Acc:{current_acc:.5f}',
