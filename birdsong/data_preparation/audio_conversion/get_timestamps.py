05ee55d17b8c4f22713a0c3141eae89fc6dfa4eb
Falk Van der Meirsch <falk@codedor.be>
2019-03-11 12:33:12 +0100

    change storage location, typos and query fix

--- birdsong/data_preparation/audio_conversion/get_timestamps.py
+++ birdsong/data_preparation/audio_conversion/get_timestamps.py
@@ -10,11 +10,11 @@ import sqlite3
 
 if 'HOSTNAME' in os.environ:
     # script runs on server
-    STORAGE_DIR = '/storage/top10_german_birds/'
+    STORAGE_DIR = '/storage/all_german_birds/'
     DATABASE_DIR = '/storage/db.sqlite'
 else:
     # script runs locally
-    STORAGE_DIR = 'storage/top10_german_birds/'
+    STORAGE_DIR = 'storage/all_german_birds/'
     DATABASE_DIR = 'storage/db.sqlite'
 
 # Get a list of files that are downloaded
@@ -25,16 +25,16 @@ print(len(downloaded_files))
 # Get the recording ID's from the filenames
 downloaded_ids = [int(x[:-4]) for x in downloaded_files]
 
-# Get all the recordings that were already precessed before
+# Get all the recordings that were already processed before
 conn = sqlite3.connect(DATABASE_DIR)
 print('database loaded')
 c = conn.cursor()
-c.execute("""select id from recordings where downloaded = 1.0""")
+c.execute("""select id from recordings where duration IS NOT NULL""")
 processed_ids = [i[0] for i in c.fetchall()]
 print('list of already processed recordings')
 print(len(processed_ids))
 
-# Remove the alreadey processed recordings from the ones we want to process
+# Remove the already processed recordings from the ones we want to process
 to_process = [x for x in downloaded_ids if x not in processed_ids]
 print('list of files to process')
 print(len(to_process))
@@ -50,9 +50,11 @@ for i, rec_id in enumerate(to_process):
         batch.append((duration, sum_signal, timestamps, rec_id))
         if len(batch) % 10 == 0:
             print(f"batch {i} full")
-            c.executemany("""UPDATE recordings SET downloaded = 1.0, duration = ?, sum_signal = ?, timestamps = ? 
+            c.executemany("""UPDATE recordings SET duration = ?, sum_signal = ?, timestamps = ? 
                 WHERE id = ?""", batch)
             conn.commit()
             batch = []
     except:
         pass
+
+conn.close()
