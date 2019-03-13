5c14317366ee44c46886d3902f92120f385cab7f
bimtauer <ti7822ba-s@student.lu.se>
2019-03-13 17:49:29 +0100

    Adapt class size to 324

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ Created on Tue Mar  5 12:29:19 2019
 #!/usr/bin/env python
 
 INPUTS = {'MODEL' : 'Bulbul',
-          'BATCHSIZE' : 30,
+          'BATCHSIZE' : 128,
           'OPTIMIZER' : 'Adam',
           'EPOCHS' : 50,
-          'CLASSES' : 10,
+          'CLASSES' : 324,
           'LR' : 0.001,
           }
