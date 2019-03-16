Enable tensorboard writer and fix loss computation

birdsong/training/evaluate.py
@@ -17,7 +17,6 @@ def evaluate(model, data_loader, criterion, num_classes, DEVICE):
     model = model.to(DEVICE)
 
     n_correct = 0
-    n_total = 0
     loss = 0
 
     with torch.no_grad():
@@ -33,7 +32,6 @@ def evaluate(model, data_loader, criterion, num_classes, DEVICE):
 
             pred = output.data.max(1, keepdim=True)[1]
             n_correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
-            n_total += len(target)
 
             if batch_idx == 0:
                 pred_cat = pred
@@ -43,10 +41,8 @@ def evaluate(model, data_loader, criterion, num_classes, DEVICE):
                 targ_cat = torch.cat((targ_cat, target))
 
     conf_matrix = calc_conf_mat(pred_cat, targ_cat, num_classes)
-    #from sklearn
-    #conf_matrix = metrics.confusion_matrix(pred_cat.view(-1), targ_cat.view(-1))
 
-    loss /= (len(data_loader.dataset))
-    acc = n_correct / n_total
+    loss /= len(data_loader)
+    acc = n_correct / len(data_loader.dataset)
 
     return (loss, acc), conf_matrix

train_precomputed.py
@@ -17,7 +17,7 @@ import torch.optim as optim
 import pandas as pd
 from tensorboardX import SummaryWriter
 from training import train, evaluate, logger, plot_conf_mat
-from datasets.sequential import RandomSpectralDataset, SpectralDataset
+from datasets.sequential import SpectralDataset
 from datasets.tools.enhancement import exponent
 
 if 'HOSTNAME' in os.environ:
@@ -33,9 +33,9 @@ else:
 
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-PIN = True if torch.cuda.is_available() else False
+PIN = torch.cuda.is_available()
 
-print(DEVICE)
+print(f'Training on {DEVICE}')
 
 
 def main(config_file):
@@ -91,15 +91,15 @@ def main(config_file):
         }, is_best, filename=state_fname)
 
         img_path = log_path + '/train' + '_' + str(epoch) + '.png'
-        img = plot_conf_mat(img_path, train_conf_matrix) 
+        img = plot_conf_mat(img_path, train_conf_matrix)
 
         img_path = log_path + '/test' + '_' + str(epoch) + '.png'
         img = plot_conf_mat(img_path, test_conf_matrix)
 
-        #logger.write_summary(writer, epoch, train_stats, test_stats, img)
+        logger.write_summary(writer, epoch, train_stats, test_stats, img)
         logger.dump_log_txt(date, start_time, local_config, train_stats, test_stats, best_acc, log_fname)
 
-    #writer.close()
+    writer.close()
     print('Finished Training')
 
 