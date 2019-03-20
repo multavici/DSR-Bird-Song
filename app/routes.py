87556f2420d9625f2a4f6f8beed178aaf94ca23a
Falk Van der Meirsch <multavici@gmail.com>
2019-03-20 17:31:40 +0100

    add predict API and js call

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,9 @@ def index():
 
 
 app.run(host='0.0.0.0', port=50000)
+
+@app.route("/classify", methods=['POST'])
+def classify():
+    #audio = request.form['audio']
+    pred = "Plegadis falcinellus"
+    return jsonify({'species': pred})
