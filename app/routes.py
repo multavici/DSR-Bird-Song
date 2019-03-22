0f1bb58093f7a2cebdd2b082c8088c8e072b6930
Falk Van der Meirsch <multavici@gmail.com>
2019-03-22 18:41:44 +0100

    preparation for prediction function

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, jsonify, request
 import librosa
+import torch
 
 app = Flask(__name__)
 
+# load the model from checkpoint
+# checkpoint_path = 'model/checkpoint.tar'
+# checkpoint = torch.load(checkpoint_path, map_location='cpu')
+
+# model = checkpoint['model']
+# state = checkpoint['state_dict']
+# label_dict = checkpoint['label_dict']
+
+# model.load_state_dict(state)
 
 @app.route("/")
 def index():
@@ -17,8 +27,12 @@ def classify():
     y, sr = librosa.load('temp/audio.webm')
     print(y)
     print(sr)
-    # TODO: Add prediction function
+    # TODO: Add slicing function
+    slice_ = "TODO"
 
+    # make prediction
+    #encoded_pred = model(torch.tensor(slice_).float()).argmax()
+    #pred = label_dict[encoded_pred]
 
     pred = "Plegadis falcinellus"
     return jsonify({
@@ -27,4 +41,4 @@ def classify():
         })
 
 #if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=8000)
+#    app.run()
