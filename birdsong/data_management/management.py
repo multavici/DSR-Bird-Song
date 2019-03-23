a75193f811c253243150255b4d9578690ea559d8
bimtauer <ti7822ba-s@student.lu.se>
2019-03-23 15:35:20 +0100

    download_missing get list of tuples, need to fix negative missing

--- birdsong/data_management/management.py
+++ birdsong/data_management/management.py
@@ -1,5 +1,4 @@
 import os
-from urllib.request import urlcleanup
 from .utils.balanced_split import make_split
 from .utils import sql_selectors
 from .precomputing_slices import Slicer
@@ -40,7 +39,6 @@ class DatabaseManager(object):
     def make_selection(self, nr_of_classes=100, slices_per_class=1200):
         self.Selection = Selection(self.conn, nr_of_classes, slices_per_class)
 
-        
     def selection_df(self):
         """ Based on the classes in the current selection return a dataframe with 
         label and path for each slice available for these classes. Alert the user 
@@ -68,41 +66,35 @@ class DatabaseManager(object):
         df = pd.DataFrame(list_recs, columns=['path', 'label'])
         return df
                 
-    def clean_db(self):
-        """ Reset the 'downloaded' column in the recordings table and update it
-        again for all recordings for which slices exist in 'signal_dir'. """
-        rec_id_list = list(self.slices_per_downloaded_recording().keys())
-        sql_selectors.reset_downloaded(self.conn)
-        sql_selectors.set_downloaded(self.conn, rec_id_list)
-
-
     def slices_per_species_downloaded(self):
         """ Retrieves Dataframe with class names for currently available slices
         and groups by class """
         df = self.inventory_df().rename(columns={'path':'downloaded_slices'})
         return df.groupby('label').downloaded_slices.count().astype(int).sort_values().to_frame()
-        
-    def download_missing(self):
-        # reimplement
-        
-        to_download = self.Selection.missing_recordings
-        if len(to_download) == 0:
-            print('Nothing to download, Selection compltete.')
-            return
-        
-        self._download_threaded(to_download)
-        # Log update of slices:
-        new_balances = self.slices_per_species_downloaded()
-        differences = new_balances - balances
-        print(differences[differences > 0])
     
-    def _download_threaded(self, recordings):
+    def slices_per_downloaded_recording(self):
+        """ Returns dictionary of downloaded recordings' rec_ids and the number 
+        of signal slices made from them. """
+        files = os.listdir(self.signal_dir)
+        rec_ids = [file.split('_')[0] for file in files]
+        return {k: v for k,v in zip(Counter(rec_ids).keys(), Counter(rec_ids).values())}
+    
+    def download_missing(self):
+        already_available = self.slices_per_species_downloaded()
+        missing_slices = self.Selection.assess_missing(already_available)
+        to_download = []
+        for row in missing_slices.itertuples(name=False):
+            print(row)
+            to_download += sql_selectors.recordings_to_download(self.conn, row[0], row[1])
+        return to_download
+    
+    def _download_threaded(self, ChosenSlicer, recordings, update_db=True):
         # Handle recordings in bunches of 24 to avoid filling tmp too much:
         at_a_time = 24
         total = len(recordings)
         print(f'Downloading {total} recording(s)')
         for iteration, bunch in enumerate([recordings[i:i+at_a_time] for i in range(0, len(recordings), at_a_time)]):
-            self.SignalSlicer(bunch)
+            ChosenSlicer(bunch)
             percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
             filledLength = int(100 * iteration // total)
             bar = fill * filledLength + '-' * (100 - filledLength)
@@ -112,50 +104,36 @@ class DatabaseManager(object):
         
         print('Done downloading!')
         # Update DB:
-        rec_ids_to_download = list(map((lambda x: str(x[0])), recordings))
-        sql_selectors.set_downloaded(self.conn, rec_ids_to_download)
+        if update_db:
+            rec_ids_to_download = list(map((lambda x: str(x[0])), recordings))
+            sql_selectors.set_downloaded(self.conn, rec_ids_to_download)
         return
     
-    def seconds_per_species_local_remote(self):
-        """ This compares the total seconds of audio material available for each
-        species that has already been downloaded vs what is still available.   
-        """
-        downloaded = sql_selectors.downloaded_german_recordings(self.conn)
-        downloaded['downloaded'] = 1
-        
-        not_downloaded = sql_selectors.not_downloaded_german_recordings(self.conn)
-        not_downloaded['downloaded'] = 0
-        df = pd.concat([downloaded, not_downloaded])
-        return df
-
     def resample_df(self, df, samples_per_class):
         """ Up- or downsample a dataframe by randomly picking a fixed number of 
         samples for each class """
         out = df.groupby('label').apply(lambda x: x.sample(n=samples_per_class, replace=True)).reset_index(drop=True)
         return out.sample(frac=1).reset_index(drop=True)
     
-    def slices_per_downloaded_recording(self):
-        files = os.listdir(self.signal_dir)
-        rec_ids = [file.split('_')[0] for file in files]
-        return {k: v for k,v in zip(Counter(rec_ids).keys(), Counter(rec_ids).values())}
+    def clean_db(self):
+        """ Reset the 'downloaded' column in the recordings table and update it
+        again for all recordings for which slices exist in 'signal_dir'. """
+        rec_id_list = list(self.slices_per_downloaded_recording().keys())
+        sql_selectors.reset_downloaded(self.conn)
+        sql_selectors.set_downloaded(self.conn, rec_id_list)
+
         
     def make_some_noise(self):
-        balances = self.slices_per_species_downloaded()
-        labels = balances.index.values
-            
-        # Get noise for all species currently downloaded
+        labels = self.Selection.classes_in_selection.index.values
+        
+        # Get noise for all species in current selection
         recordings = []
         for label in labels:
             recordings_for_class = sql_selectors.recordings_for_noise(self.conn, label, 1)
             recordings += recordings_for_class
-        print(f'Selected {len(recordings)} recordings for noise slicing')
-        rec_ids_to_download = list(map((lambda x: str(x[0])), recordings))
+    
+        self._download_threaded(self.NoiseSlicer, recordings, update_db=False)
         
-        # Handle recordings in bunches of 24 to avoid filling tmp too much:
-        at_a_time = 24
-        for bunch in [recordings[i:i+at_a_time] for i in range(0, len(recordings), at_a_time)]:
-            self.NoiseSlicer(bunch)
-            urlcleanup()
     
     """        
     def _plot_slices_before_after_downloading(self, balances, differences):
