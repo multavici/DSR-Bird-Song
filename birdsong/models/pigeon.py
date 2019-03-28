8b0d51267b5fbf574dc48267946e617ce075bf70
bimtauer <ti7822ba-s@student.lu.se>
2019-03-28 15:10:35 +0100

    Fix Pigeon pooling stride

--- birdsong/models/pigeon.py
+++ birdsong/models/pigeon.py
@@ -32,19 +32,19 @@ class Pigeon(nn.Module):
         self.summ1 = nn.Sequential(
             nn.Conv2d(32, 32, kernel_size=(30, 1), stride=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride=2)
+            nn.MaxPool2d(kernel_size=(1,2), stride=(1,2))
             )
         
         self.summ2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=(1, 3), stride=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride=2)
+            nn.MaxPool2d(kernel_size=(1,2), stride=(1,2))
             )
         
         self.summ3 = nn.Sequential(
             nn.Conv2d(64, no_classes, kernel_size=(1, 3), stride=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,3), stride=2)
+            nn.MaxPool2d(kernel_size=(1,3), stride=(1,3))
             )
 
     def forward(self, x):
