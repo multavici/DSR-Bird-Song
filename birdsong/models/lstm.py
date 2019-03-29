7e5bd38a98138653118d9adae39b680b17bb9d3d
bimtauer <ti7822ba-s@student.lu.se>
2019-03-29 18:29:06 +0100

    New lstm architecture with rhythmic and timbral features

--- birdsong/models/lstm.py
+++ birdsong/models/lstm.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 20 16:59:17 2019
-
-@author: ssharma
-"""
-
 import torch
-import torch.nn as nn
-# Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from torch import nn
+
 
 class LstmModel(nn.Module):
 
@@ -20,43 +11,41 @@ class LstmModel(nn.Module):
         self.time_axis = time_axis
         self.no_classes = no_classes
         
-        self.input_features = 256 #input_dim
-        self.seq_length = 216 
+        self.input_features = 1622 #input_dim
+        self.seq_length = 66 
         
         # Hyper parameters
         # Hidden dimensions and number of hidden layers
         self.hidden_dim = 200 #500
         self.layer_dim = 3 #7
         
-        self.harmony = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=(5,3), stride=1),
-            #nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.MaxPool2d(kernel_size=(3,2), stride=(3,2)),
-            
-            nn.Conv2d(16, 32, kernel_size=(5,3), stride=1),
-            #nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.MaxPool2d(kernel_size=(3,2), stride=(3,2)),
-            
-            nn.Conv2d(32, 64, kernel_size=(3,1), stride=1),
-            #nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.MaxPool2d(kernel_size=(3,1), stride=(3,1)),
-            
-            nn.Conv2d(64, 64, kernel_size=(3,1), stride=1),
-            #nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.MaxPool2d(kernel_size=(3,1), stride=(3,1)),
-            
-            nn.Conv2d(64, 64, kernel_size=(2,1), stride=1),
-            #nn.BatchNorm2d(64),
-            nn.ReLU(),
-            )
+        
+        self.timbral_features = nn.Sequential(
+                    nn.Conv2d(1, 16, kernel_size=(9,3), stride=1),
+                    nn.BatchNorm2d(16),
+                    nn.ReLU(),
+                    #nn.Dropout(0.3),
+
+                    nn.Conv2d(16, 32, kernel_size=(5,3), stride=1),
+                    #nn.BatchNorm2d(32),
+                    nn.ReLU(),
+                    nn.Dropout(0.3),
+                    nn.MaxPool2d(kernel_size=(3,3), stride=(3,3)),
+
+
+                    nn.Conv2d(32, 64, kernel_size=5, stride=1),
+                    #nn.BatchNorm2d(64),
+                    nn.ReLU(),
+                    nn.Dropout(0.3),
+                    nn.MaxPool2d(kernel_size=(3,1), stride=(3,1)),
+                    )
+
+          
+        
+        self.rhythm = nn.Sequential(
+                    nn.AvgPool2d(kernel_size=(150,19), stride=(5,3))
+                    )
+
         
         # batch_first=True shapes Tensors : batch_dim, seq_dim, feature_dim)
         self.lstm = nn.LSTM(self.input_features, self.hidden_dim, self.layer_dim, dropout=0.5, batch_first=True)
@@ -64,26 +53,23 @@ class LstmModel(nn.Module):
         
         self.fc = nn.Linear(self.hidden_dim, self.no_classes)
         
-    def forward(self, x):
-        #out =  self.harmony(x).squeeze(2).permute(0,2,1)   
-        #Out shape: batch_dim, 52 time_steps, 64 timbral_features
-        x = x.squeeze(1).permute(0,2,1) 
-        print(x.shape)
-        output_seq, hidden_state = self.lstm(x)
+    def forward(self, x):   
+        rhythm_out = self.rhythm(x)
+        #print(rhythm_out.shape)
+
+        rhythm_out = rhythm_out.view(rhythm_out.shape[0], -1 , 66)
+        
+        timbral_out = self.timbral_features(x)
+        #print(timbral_out.shape)
+
+        timbral_out = timbral_out.view(timbral_out.shape[0], -1 , 66)
+        stack = torch.cat((rhythm_out, timbral_out), 1).permute(0,2,1)  
+
+       
+        #LSTM input Shape: batch_dim, sequence_dim, feature_dim
+        #print(stack.shape)
+        output_seq, hidden_state = self.lstm(stack)
         last_output = output_seq[:, -1]
         out = self.fc(last_output)
         
         return out
-
-def test():
-    image = torch.randn(64, 1, 256, 216)
-    lstm = LstmModel(256, 216, 100)
-    output = lstm(image)
-    
-    print("input shape:")
-    print(image.shape)
-    print("output shape:")
-    print(output.shape)
-
-if __name__ == '__main__':
-    test()
